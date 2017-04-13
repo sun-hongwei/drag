f89cdbb5b4f9155f90ac538e90791300266270ba
sun-hongwei <1012191814@qq.com>
2017-04-13 23:53:18 +0800

    测试文件提交

---
+++ User.java
@@ -0,0 +1,34 @@
+package com.sun.bean;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+	
+	private String user_id;
+	private String user_name;
+	private int user_age;
+	
+	
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public int getUser_age() {
+		return user_age;
+	}
+	public void setUser_age(int user_age) {
+		this.user_age = user_age;
+	}
+	
+	
+	
+	
+}
